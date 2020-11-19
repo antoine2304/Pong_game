856a93664c1c719d3528e6de33a34fad8a5c84ec
antoine2304 <63273266+antoine2304@users.noreply.github.com>
2020-11-19 11:05:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("hello world, I'm back for more!")
-# Changes made with branch test2
-print("Lol de type mineur")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((960, 760))
+pygame.display.set_caption('Pong')
+
+
+class Plaquette:
+    
+    def __init__(self, image):
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.s_rect = screen.get_rect()
+        self.screen = screen
+
+    def blit(self):
+        self.screen.blit(self.image, self.rect)
